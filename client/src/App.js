446ecc72d27c5d0c75dd57deecd7a0498e18433d
Keaton Sentak <k.sentak@outlook.com>
2020-11-07 10:54:50 -0500

    convert tabs to spaces for consistency

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 const App = () => {
-	return (
-		<div className="App">
-			<h1>Welcome to your new MERN app</h1>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <h1>Welcome to your new MERN app</h1>
+    </div>
+  );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -15,16 +15,16 @@ app.use(express.json());
 const connection_url = process.env.MONGODB_CONNECTION;
 
 mongoose
-	.connect(connection_url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(() => {
-		console.log('MongoDB Connected!');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB Connected!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Production site variable
 // Change to 1 if the app is being deployed into a production environment
@@ -32,16 +32,16 @@ const liveSite = 0;
 
 // Uses the production build of the React front-end if the app is being deployed into a production environment
 if (liveSite === 1) {
-	// Express will serve up production assets
-	app.use(express.static('client/build'));
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
 
-	// Express serves up index.html file if it doesn't recognize route
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-	});
+  // Express serves up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
 }
 
 // App listening
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
